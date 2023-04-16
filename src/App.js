add74c433709f87e7ebc09d399f522dea5642701
Edin Mavric <edinmavric10@gmail.com>
2023-04-16 17:51:30 +0200

    displaying card data using map function

--- src/App.js
+++ src/App.js
@@ -6,13 +6,50 @@ import Footer from './components/Footer/Footer';
 import Card from './components/card/card';
 import Form from './components/registrationForm/registrationForm';
 
+const cardData = [
+  {
+    id: 1,
+    rating: "4.8(227 review's)",
+    distance: '19km from center',
+    title: 'Hotel Hotel',
+    price: '702',
+    color: '#dedec8',
+  },
+  {
+    id: 2,
+    rating: "3.9(311 review's)",
+    distance: '12km from center',
+    title: 'Hotel Motel',
+    price: '455',
+    color: '#debdbd',
+  },
+  {
+    id: 3,
+    rating: "4.2(198 review's)",
+    distance: '6km from center',
+    title: 'Motel Hotel',
+    price: '520',
+    color: '#dedec8',
+  },
+];
+
 const App = () => {
   return (
     <>
       <Header title="Edin" li1="About" li2="Counter" />
       <Counter sub="-" add="+" />
       <div className="card-container">
-        <Card
+        {cardData.map(card => (
+          <Card
+            key={card.id}
+            rating={card.rating}
+            distance={card.distance}
+            title={card.title}
+            price={card.price}
+            color={card.color}
+          />
+        ))}
+        {/* <Card
           rating="4.8(227 review's)"
           distance="19km from center"
           title="Hotel Hotel"
@@ -32,7 +69,7 @@ const App = () => {
           title="Motel Hotel"
           price="520"
           color="#dedec8"
-        />
+        /> */}
       </div>
       <Form />
       <Footer paragraph="Thanks for using our Counter!" />
